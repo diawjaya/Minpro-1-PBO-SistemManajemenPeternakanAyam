b5b30e9656ab02e2f91936e06444573ba1fc3dc4
diawjaya <magicsausagegd@gmail.com>
2026-09-03 13:47:00 +0800

    Update PBODua.java

--- src/main/java/com/mycompany/pbo/dua/PBODua.java
+++ src/main/java/com/mycompany/pbo/dua/PBODua.java
@@ -11,6 +11,6 @@ package com.mycompany.pbo.dua;
 public class PBODua {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Heawd!");
     }
 }
